ed618a71c8451f07c472478440f92bc751e3a74d
rubenkristian96 <rubenkristian96@gmail.com>
2024-06-10 13:01:43 +0700

    fix add room

--- app/controllers/room_chat_controller.rb
+++ app/controllers/room_chat_controller.rb
@@ -1,25 +1,25 @@
 class RoomChatController < ApplicationController
     def index
-        @room_chat = RoomChat.order(updated_at: :desc).page(params[:page]).per(params[:size])
-        render json: @room_chat
+        room_chat = RoomChat.order(updated_at: :desc).page(params[:page]).per(params[:size])
+        render json: { data: room_chat, current_page: room_chat.current_page, total_pages: room_chat.total_pages, total_count: room_chat.total_count }
     end
 
     def show
-        room = RoomChat.find(:room_id)
+        room = RoomChat.find(params[:room_id])
 
         if room
-            render json: { status: 'found', message: 'room founded', data: room }, status: :success
+            render json: { status: 'found', message: 'room founded', data: room }, status: :ok
         else
             render json: { status: 'not found', messag: 'room is not found'}, status: :not_found
         end
     end
 
     def add
-        room = params.require(:name, :creator)
-        @room_chat = RoomChat.new(room)
+        room = params.permit(:name, :creator)
+        room_chat = RoomChat.new(room)
 
-        if @room_chat.save
-            render json: { status: 'success', message: 'room created', data: room }, status: :created
+        if room_chat.save
+            render json: { status: 'success', message: 'room created', data: room_chat }, status: :created
         else
             render json: { status: 'failed', message: 'room not created'}, status: :unprocessable_entity
         end
