1976e764e5c58b2684cf3a765d7cf4d5dfd1872b
rubenkristian96 <rubenkristian96@gmail.com>
2024-06-10 12:55:04 +0700

    test message controller

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    room = RoomChat.create(name: "Hello", creator: "1234")
+    message1 = Message.create(body: "Message 1", room_chat_id: room.id)
+    message2 = Message.create(body: "Message 2", room_chat_id: room.id)
+    get messages_url, params: { room_id: room.id }
+    assert_response :success
+  end
 end
